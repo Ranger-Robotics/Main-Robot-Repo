8cfdc4ae56c81db6fb956eed7743ec67dde386e9
Jordan <jordanayan@live.com>
2018-01-25 14:43:36 -0600

    added "startingPosition" varible
    Mostly just testing Github desktop

--- TankDrive_V5.java
+++ TankDrive_V5.java
@@ -16,8 +16,8 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 
 public class Robot extends TimedRobot {
-	
-	
+
+
 	private SpeedController motor0 = new Talon(0); // initialize the motor as a
 	private SpeedController motor1 = new Talon(1);
 	private SpeedController motor2 = new Talon(2);
@@ -30,14 +30,15 @@ public class Robot extends TimedRobot {
 	double DZ = .1;
 	double SpeedModifier = 1;
 	double liftSpeedMod;
-	
+	String startingPosition = "left";
+
 
 	@Override
 	public void robotInit() {
-		
-		
+
+
 	}
-	
+
 	void turnRight() {
 		setLeft(0.3);
 		setRight(0.3);
@@ -45,18 +46,18 @@ public class Robot extends TimedRobot {
 		setLeft(0);
 		setRight(0);
 	}
-	
+
 	void setLeft(double value) {
 		value = value * 0.846;
 		motor0.set(value);
 		motor1.set(value);
 	}
-	
+
 	void setRight(double value) {
 		motor2.set(value);
 		motor3.set(value);
 	}
-	
+
 	void setLift(double value) {
 		motor4.set(value);
 	}
@@ -69,14 +70,14 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void disabledPeriodic() {
-		
+
 	}
 
 
 	@Override
 	public void autonomousInit() {
 		String gameData = DriverStation.getInstance().getGameSpecificMessage();
-		
+
 		if(gameData.charAt(0) == 'L') {
 			double autoModeSpeed = 0.3;
 			setLeft(autoModeSpeed);
@@ -91,60 +92,60 @@ public class Robot extends TimedRobot {
 		}
 	}
 
-	
+
 	@Override
 	public void autonomousPeriodic() {
-		
+
 	}
 
 	@Override
 	public void teleopInit() {
-		
+
 	}
 
 
 	@Override
 	public void teleopPeriodic() {
-			
-			
-			
+
+
+
 			if (stick.getY() < -DZ) {
 				setLeft(-stick.getY()*SpeedModifier);
 
-			} else if (stick.getY() > DZ) { 
+			} else if (stick.getY() > DZ) {
 				setLeft(-stick.getY()*SpeedModifier);
-				
+
 			} else {
 				setLeft(0);
 			}
-			
+
 			if  (stick.getRawAxis(5) < -DZ) {
 				setRight(stick.getRawAxis(5)*SpeedModifier);
-				
+
 			} else if (stick.getRawAxis(5) > DZ) {
 				setRight(stick.getRawAxis(5)*SpeedModifier);
-				
+
 			} else if (stick.getRawAxis(3) > 0.5) {
 				setLeft(1*SpeedModifier);
 				setRight(-1*SpeedModifier);
-				
+
 			} else {
-				
+
 				setRight(0);
 			}
-			
+
 			if (stick.getRawButton(6)) {
 		    	SpeedModifier = 0.25;
 			} else {
 				SpeedModifier = 1;
 			}
-			
+
 			liftSpeedMod = stick2.getRawAxis(3);
-			
+
 			if (stick2.getRawAxis(1) < -0.2) {
 				setLift(stick2.getRawAxis(1));
 			}
-			
+
 			if (stick2.getRawAxis(1) > 0.2) {
 				setLift(stick2.getRawAxis(1));
 			}
